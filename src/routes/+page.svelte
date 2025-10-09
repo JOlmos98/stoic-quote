2f4f78d4646981ca8e2f2681607f4340d2a30eef
JOlmos98 <soler98@hotmail.es>
2025-10-09 19:32:00 +0200

    refactor: incluidas librerías útiles para un futuro.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+
+
+
+
+
+</script>
+
+<div class="flex flex-col items-center justify-center bg-amber-100">
+	<h1>Hello %paraglide.lang%</h1>
+	<p>This is a paragraph</p>
+	<p>This is another paragraph</p>
+	<p>This is a third paragraph</p>
+
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
